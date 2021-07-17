2a66441f73b73c330f03bb40fe0ed840386222e0
Jacob Tung <zh3nd0ng@gmail.com>
2021-07-18 00:04:08 +0800

    add screenshot keybind

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -78,6 +78,7 @@ myKeys =
         , ("M-S-b", spawn "~/.local/bin/Bitwarden*.AppImage --no-sandbox %U")
         , ("M-S-e", spawn "thunar")
         , ("M-S-s", spawn "spotify")
+        , ("<Print>", spawn "screenshot.sh")
     -- Kill windows
         , ("M-S-c", kill)     -- Kill the currently focused client
     -- Windows navigation
