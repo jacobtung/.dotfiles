2bf30617f17a9bb9508718a320bda832dae87346
Jacob Tung <zh3nd0ng@gmail.com>
2021-09-03 15:10:29 +0800

    delete not used scripts

--- scripts/.local/scripts/screenshot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# lockscreen scripts cause of i3 exec not good
-import /tmp/latest-screenshot.png
-sreenshottime=$(date +"%Y-%m-%d-%H:%M:%S")
-cp -n /tmp/latest-screenshot.png /home/jacob/Pictures/Screenshots/screenshot-$sreenshottime.png
