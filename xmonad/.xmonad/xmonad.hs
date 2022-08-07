328e5df1ce596d6dcd53f4f3c07b28a9128ee2dd
Jacob Tung <zh3nd0ng@gmail.com>
2022-08-07 09:37:39 +0800

    add neofetch when xmonad xterm start

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -131,7 +131,7 @@ projects =
                 
     , Project   { projectName       = "CLI"
                 , projectDirectory  = "~/"
-                , projectStartHook  = Just $ do spawnOnOnce "CLI" "xterm"
+                , projectStartHook  = Just $ do spawnOnOnce "CLI" "xterm -e neofetch"
                 }
     ]
 
